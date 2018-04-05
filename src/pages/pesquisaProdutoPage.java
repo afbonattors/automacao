c4578acbeace17e0db7aae1c63f95be4d9c94e5b
Andre Bonatto <afbonatto@gmail.com>
2018-04-05 14:55:55 -0300

    Selenium WebDriver

--- src/pages/pesquisaProdutoPage.java
+++ src/pages/pesquisaProdutoPage.java
@@ -5,11 +5,17 @@ import org.openqa.selenium.WebElement;
 
 import utils.baseTest;
 
-public class pesquisaProduto {
+public class pesquisaProdutoPage {
 	
-	public static WebElement clicaLupa() {
+	public static WebElement lLupaPesquisa() {
 		
 		return baseTest.getDriver().findElement(By.cssSelector("#busca-mobile > span > button"));
+		
+	}
+	
+	public static WebElement gridProdutos() {
+		
+		return baseTest.getDriver().findElement(By.cssSelector("#colmd9-search-percent > div.produtos > div.list_prod_result.clearfix.custom-result-list"));
 		
 	}
 	
---
+++ src/scripts/pesquisaProdutoScript.java
@@ -0,0 +1,15 @@
+package scripts;
+
+import pages.pesquisaProdutoPage;
+
+public class pesquisaProdutoScript extends pesquisaProdutoPage {
+	
+	public static void clicaLupa() {
+		
+		lLupaPesquisa().click();
+		
+	}
+	
+	public static void 
+
+}
