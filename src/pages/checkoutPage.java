b94b84e4292235c2b917fb8452f9ff55aaed4256
Andre Bonatto <afbonatto@gmail.com>
2018-04-06 10:34:34 -0300

    Selenium WebDriver

---
+++ src/pages/checkoutPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.baseTest;
+
+public class checkoutPage extends baseTest{
+	
+	public static WebElement finalizaPedido() {
+		
+		return baseTest.getDriver().findElement(By.cssSelector("body > div.wrapper > header > div > div.buscaecarrinho.ng-scope > ul > li.carrinho.act > div > div.total > a.lkfinalizar"));
+		
+	}
+	
+	public static WebElement produtoCarrinho() {
+		
+		return baseTest.getDriver().findElement(By.cssSelector(""));
+		
+	}
+
+}
