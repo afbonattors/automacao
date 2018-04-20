214e05f0990fdeba78012a21a48eacfc7b8831b9
Andre Bonatto <afbonatto@gmail.com>
2018-04-20 17:28:10 -0300

    Selenium WebDriver

---
+++ src/scripts/nossasLojasScript.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import pages.homePage;
+
+public class nossasLojasScript extends homePage {
+	
+	public static void acessaNossasLojas() {
+
+		nossasLojas().click();
+	
+	}
+
+}
