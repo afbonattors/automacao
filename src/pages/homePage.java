f327ea4db8f6cba7b9db06f3d46e712ee7f5ea21
Andre Bonatto <afbonatto@gmail.com>
2018-04-25 15:13:27 -0300

    Selenium WebDriver

--- src/pages/homePage.java
+++ src/pages/homePage.java
@@ -136,5 +136,58 @@ public class homePage extends baseTest {
         return baseTest.getDriver().findElement(By.cssSelector("body > div.wrapper > header > div > nav > ul > li:nth-child(10) > h2 > a"));
 
     }
+    
+    public static WebElement footerHome() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer"));
+    	
+    }
+    
+    public static WebElement divSaibaMaisFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[1]"));
+    }
+    
+    public static WebElement divCentralAtendimentoFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[2]"));
+    	
+    }
+    
+    public static WebElement divServicosFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[3]"));
+    	
+    }
+    
+    public static WebElement divSegurancaPrivacidadeFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[4]"));
+    	
+    }
+    
+    public static WebElement divTelefonesFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[5]"));
+    	
+    }
+    
+    public static WebElement divTelevendasFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[6]"));
+    	
+    }
+    
+    public static WebElement divAcompanheEtnaFooter() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[1]/div[7]"));
+    	
+    }
+    
+    public static WebElement divTextoRodape() {
+    	
+    	return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/footer/div[2]"));
+    	
+    }
 
 }
