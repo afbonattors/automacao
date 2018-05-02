babd42036a5dc3b5a31cbb6fb3fc3581f85ce421
Andre Bonatto <afbonatto@gmail.com>
2018-05-02 15:48:55 -0300

    Selenium WebDriver

---
+++ src/pages/atendimentoLojaFisicaPage.java
@@ -0,0 +1,93 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.baseTest;
+
+public class atendimentoLojaFisicaPage extends baseTest {
+	
+	public static WebElement divTelefones() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[1]"));
+		
+	}
+	
+	public static WebElement divHorarioAtendimento() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[1]/p[1]"));
+		
+	}
+	
+	public static WebElement divTelefoneLocal() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[1]/p[2]"));
+		
+	}
+	
+	public static WebElement divTelefoneDemais() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[1]/p[3]"));
+		
+	}
+	
+	public static WebElement divChat() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[1]/a/img"));
+		
+	}
+	
+	public static WebElement divAcessoRapido() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[2]"));
+		
+	}
+	
+	public static WebElement divNossasLojas() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[2]/div[2]/ul/li[1]"));
+		
+	}
+	
+	public static WebElement divPoliticas() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[2]/div[2]/ul/li[2]"));
+		
+	}
+	
+	public static WebElement divDuvidas() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[2]/div[2]/ul/li[3]"));
+		
+	}
+	
+	public static WebElement divFormasPagamento() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div[2]/div[2]/ul/li[4]"));
+		
+	}
+	
+	public static WebElement txtNome() {
+		
+		return baseTest.getDriver().findElement(By.xpath("//*[@id=\"formStoreSupport\"]/div[3]/div[1]/div[1]"));
+		
+	}
+	
+	public static WebElement txtSobreNome() {
+		
+		return baseTest.getDriver().findElement(By.xpath("//*[@id=\"formStoreSupport\"]/div[3]/div[1]/div[2]"));
+		
+	}
+	
+	public static WebElement txtCPFCNPJ() {
+		
+		return baseTest.getDriver().findElement(By.xpath("//*[@id=\"formStoreSupport\"]/div[3]/div[2]/div[1]"));
+		
+	}
+	
+	public static WebElement txtTelefone() {
+		
+		return baseTest.getDriver().findElement(By.xpath("//*[@id=\"formStoreSupport\"]/div[3]/div[2]/div[2]"));
+	}
+
+}
