6ccf37b20b5d09b908fe4c9ee2a6de2b320fc708
Andre Bonatto <afbonatto@gmail.com>
2018-05-03 15:10:51 -0300

    Selenium WebDriver

--- src/pages/regulamentoCampanhasPage.java
+++ src/pages/regulamentoCampanhasPage.java
@@ -13,6 +13,36 @@ public class regulamentoCampanhasPage extends baseTest {
 		
 	}
 	
+	public static WebElement promoLista1() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/nav/ul/li[2]"));
+		
+	}
+	
+	public static WebElement promoLista2() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/nav/ul/li[3]"));
+		
+	}
+	
+	public static WebElement promoLista3() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/nav/ul/li[4]"));
+		
+	}
+	
+	public static WebElement promoLista4() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/nav/ul/li[5]"));
+		
+	}
+	
+	public static WebElement promoLista5() {
+		
+		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/nav/ul/li[6]"));
+		
+	}
+	
 	public static WebElement divRegulamento() {
 		
 		return baseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[2]/section/div"));
