faaa94b38c4d69316d8cd880390d78362e133349
Andre Bonatto <afbonatto@gmail.com>
2018-05-08 17:28:04 -0300

    Selenium WebDriver

--- src/pages/DicasEtna3_Page.java
+++ src/pages/DicasEtna3_Page.java
@@ -1,5 +1,34 @@
 package pages;
 
-public class DicasEtna3_Page {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.BaseTest;
+
+public class DicasEtna3_Page extends BaseTest {
+	
+	public static WebElement divBanner() {
+		
+		return BaseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[1]"));
+		
+	}
+	
+	public static WebElement divLista1() {
+		
+		return BaseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[4]/section/ul[2]/li[1]"));
+		
+	}
+	
+	public static WebElement divLista2() {
+		
+		return BaseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[4]/section/ul[2]/li[2]"));
+		
+	}
+	
+	public static WebElement divContainer() {
+		
+		return BaseTest.getDriver().findElement(By.xpath("/html/body/div[3]/div[5]"));
+		
+	}
 
 }
